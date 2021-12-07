7102f8e7ea79502a523a2b29ae57fed62205df44
Fábio Pereira <fadspereira@gmail.com>
2021-12-07 16:18:44 +0000

    base code

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Working on it. 🌲 Please come back soon 🎄 
+# PCD21 — Generative Type Design Workshop
+
+Imagine an empty Christmas tree 🌲 and a Christmas tree with decorations 🎄.
+Now the Christmas tree is a typographic skeleton, and the decoration is any shape that you use to give it form.
+That’s what we will be doing in this workshop, exploring possibilities to create unique glyphs/letters based on typographic skeletons and drawing techniques.
+We will be using p5.js, which enables all our creativity to be limited only to the imagination.
+
+## Requirements, but not really
+
+- If you are new to P5.js, please check this [brief introduction](https://github.com/baselcodes/PCD2021/tree/main/P5JS%20for%20Beginners).
+- A code editor. I don't recommend any. PS: use [VS Code](https://code.visualstudio.com/).
+- If you are in the cool team, install the [Live Server](https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer) extension for VS Code so you don't have to reload every time you update the code. Also, I like the [GitHub theme](https://marketplace.visualstudio.com/items?itemName=GitHub.github-vscode-theme).
+
+## Share your artwork / Workshop GIF
+
+At the end of the workshop, I will ask you to send through this form (insert link) your artwork. Then we can see each other's work and create a cool GIF.
---
+++ src/sketch.js
@@ -0,0 +1,143 @@
+// global variables
+let skeleton;
+let text = "";
+let epsilon = 0.5;
+const color = "#000000";
+
+let textInput, epsilonInput, resetInput;
+
+// load de skeleton
+function preload() {
+  skeleton = loadJSON("/skeletons/Scala-Italic.json");
+  console.log("skeleton: ", skeleton);
+}
+
+function setup() {
+  // create canvas
+  createCanvas(windowWidth, windowHeight);
+  pixelDensity(3);
+  frameRate(30);
+
+  // create a text input
+  textInput = createInput(text);
+  textInput.id("text-input");
+  textInput.input(updateText);
+
+  // create a epsilon input
+  epsilonInput = createSelect();
+  epsilonInput.id("epsilon-input");
+  for (let key of Object.keys(skeleton["glyphs"]).sort()) {
+    epsilonInput.option(key);
+  }
+  epsilonInput.selected(epsilon);
+  epsilonInput.changed(updateEpsilon);
+
+  // create clean button
+  resetInput = createButton("Reset");
+  resetInput.id("reset-input");
+  resetInput.mousePressed(cleanCanvas);
+}
+
+function draw() {
+  background(255);
+
+  const widthRatio = 0.7;
+  const initialXPos = 100;
+
+  let currentPosition = createVector(initialXPos, height / 2 + 100);
+
+  for (const character of text) {
+    //if finds the character on the array
+    if (Object.keys(skeleton["glyphs"][epsilon]).includes(character)) {
+      let currentCharacter = skeleton["glyphs"][epsilon][character];
+
+      makeItPop(currentPosition, currentCharacter);
+      // drawLine(currentPosition, currentCharacter);
+
+      currentPosition.add(skeleton["maxX"] * widthRatio, 0);
+    }
+    // if it is a space
+    else if (character == " ") {
+      currentPosition.add(skeleton["maxX"] * widthRatio, 0);
+    }
+  }
+}
+
+//where the magic happens
+//fill the skeleton by drawing a shape on every point
+function makeItPop(position, character) {
+  push();
+  translate(position.x, position.y);
+  fill(color);
+  noStroke();
+
+  const width = 10;
+
+  for (const line of character) {
+    for (const point of line) {
+      //take a look at all the available shapes here:
+      //https://p5js.org/reference/#group-Shape
+
+      ellipse(point.x, point.y, width, width);
+    }
+  }
+  pop();
+}
+
+//draw the skeleton
+function drawLine(position, character) {
+  push();
+  translate(position.x, position.y);
+  stroke(color);
+  noFill();
+
+  for (const line of character) {
+    beginShape();
+    for (const point of line) {
+      vertex(point.x, point.y);
+    }
+    endShape();
+  }
+  pop();
+}
+
+// utility functions
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function updateText() {
+  text = textInput.value();
+}
+
+function updateEpsilon() {
+  epsilon = epsilonInput.value();
+}
+
+function cleanCanvas() {
+  text = "";
+  textInput.value("");
+  clear();
+}
+
+// if (line.length == 2) {
+//   console.log("line: ", line);
+//   console.log("two points");
+
+//   const numPoints = 5;
+
+//   var xDist = line[1].x - line[0].x;
+//   var yDist = line[1].y - line[0].y;
+//   var dist = Math.sqrt(xDist * xDist + yDist * yDist);
+
+//   let len = dist / numPoints;
+
+//   var fractionOfTotal = len / dist;
+
+//   for (let INDEX = 0; INDEX < numPoints; INDEX++) {
+//     let x = line[0].x + xDist * (fractionOfTotal * INDEX);
+//     let y = line[0].y + yDist * (fractionOfTotal * INDEX);
+
+//     ellipse(x, y, 10, 10);
+//   }
+// }
